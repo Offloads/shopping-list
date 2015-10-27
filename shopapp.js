66e6b950b0ccfee7a378e691c757d1be2c0476f4
Nik <nikodonnell@gmail.com>
2015-10-27 10:42:28 +0100

    update from paircode with Victor

--- shopapp.js
+++ shopapp.js
@@ -1,33 +1,55 @@
 $(document).ready(
     function(){
         
+        $(document).on("click", ".delete", function() {
+                $(this).closest("li").fadeOut(200);
+            totalCost -= parseFloat($(this).data("sub"));
+            console.log($(this).data("sub"));
+            $("#totalCost").text(totalCost);
+            console.log(totalCost);
+            });
+        
+        $(document).on("click", ".done", function(){
+               $(this).closest("li").toggleClass("strike");
+            });
+        
+        var subCosts = [];
+        var totalCost = 0;
+        
+        
+        
         $('form').submit( function(event){
             event.preventDefault();
             // this is where our code goes!!!
             var item = $("input[type=text]").val();
-
-            var amount = $('input[type=number]').val();
-
-            $("#add").click(function(){
-                var $li = $('<li><span>'+ item + "(" + amount + ")" + '</span><button id="delete"      type="button">X</button><button id="done" type="button">DONE</button></li>');
+            
+            var amount = parseFloat($('input[type=number]').val());
+            var cost = $('input#itemCost').val();
+            
+            
+            cost = parseFloat(cost);
+            var subCost = amount * cost;
+            subCosts.push(subCost);
+            
+            var $li = '<li><span>'+ item + "( $" + subCost + " )" + '</span><button class="delete" data-sub="'+ subCost +'"      type="button">X</button><button class="done" type="button">DONE</button></li>';
                 
             $("ul").append($li);
-                return false;
-            });
-            
-                
-            $(document).on("click", "#delete", function() {
-                $(this).closest("li").fadeOut(200);
-            });
-            
-            //$(document).on("click", "#done", function(){
-              //  $("ul li").addClass( "strike" );
+              //  return false;
             //});
-            $(document).on("click", "#done", function () {
-        $(this).closest("li").toggleClass("strike");
-    });             
+            $("input#shopItems").val("");
+            $('input#itemAmount').val("");
+            $('input#itemCost').val("");
+            
+            
+            totalCost = subCosts.reduce(function(a,b){
+                return a+b;                           
+            },0);
+            $("#totalCost").text(totalCost);
+            
+            
+                    
     });
-        
+           
 
 });
     
